73a9bc9bf2838f1433838b903da3773983e86bf0
The_Great_Uniter <superdb117@gmail.com>
2019-10-07 21:05:12 -0400

    add final activity; link main to sub activities

--- app/src/main/java/com/example/colorfall/MainActivity.java
+++ app/src/main/java/com/example/colorfall/MainActivity.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +18,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private final int CONFIG_REQUEST = 123;
+
+    //Starts a new activity when user clicks
+    public void clickUnlock(View view)
+    {
+        Intent intent = new Intent(this,drawActivity.class);
+        startActivityForResult(intent, CONFIG_REQUEST);
+    }
+
+    public void clickUnlock(View view)
+    {
+        Intent intent = new Intent(this,galleryActivity.class);
+        startActivityForResult(intent, CONFIG_REQUEST);
+    }
+
+    public void clickUnlock(View view)
+    {
+        Intent intent = new Intent(this,TemplatesActivity.class);
+        startActivityForResult(intent, CONFIG_REQUEST);
+    }
+
+    public void clickUnlock(View view)
+    {
+        Intent intent = new Intent(this,importActivity.class);
+        startActivityForResult(intent, CONFIG_REQUEST);
+    }
+
+
 }
