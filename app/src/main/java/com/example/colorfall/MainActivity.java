bdccf9795764e983cfb9d2d66d96d59ed2a0ff14
Jonathan Giraud <46968282+Zweitonegoismus@users.noreply.github.com>
2019-10-09 14:41:08 -0400

    Test 2
    Test2

--- app/src/main/java/com/example/colorfall/MainActivity.java
+++ app/src/main/java/com/example/colorfall/MainActivity.java
@@ -39,30 +39,10 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, CONFIG_REQUEST);
     }
 
-    public void clickUnlock(View view)
-    {
-        Intent intent = new Intent(this,importActivity.class);
-        startActivityForResult(intent, CONFIG_REQUEST);
-    }
-<<<<<<< HEAD
-    public int adr (int x, int y)
-    {
-        return x+2*y;
-    }
+//    public void clickUnlock(View view)
+//    {
+//        Intent intent = new Intent(this,importActivity.class);
+//        startActivityForResult(intent, CONFIG_REQUEST);
+//    }
 
-=======
-
-<<<<<<< HEAD
-    public int addNum(int num1, int num2)
-    {
-        int result = num1 + num2;
-        return result;
-=======
-    //git merge testing, NOT a real method we need
-    public void gitMergeTesting() {
-        //code :~)
-        //gabe
->>>>>>> ad61ff115d421a3c37a3ca8325e1612dda8e432d
-    }
->>>>>>> 48ece82d693c0fa5eefe18381ab7e1642b5ac5e8
 }
