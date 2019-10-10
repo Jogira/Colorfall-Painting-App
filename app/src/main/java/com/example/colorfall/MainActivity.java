962e1d5fbcb2071893084194bcced2798cc009da
Jonathan <Zweitonegoismus@gmail.com>
2019-10-09 21:35:18 -0400

    Activity Linking and UI Update
    All of the buttons were linked to open their proper acitivities and the home screen was cleaned up and updated.

--- app/src/main/java/com/example/colorfall/MainActivity.java
+++ app/src/main/java/com/example/colorfall/MainActivity.java
@@ -12,37 +12,87 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button drawBtn;
+    private Button galleryBtn;
+    private Button templatesBtn;
+    private Button importBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawBtn = (Button) findViewById(R.id.drawBtn);
+        drawBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openDrawScreen();
+            }
+        });
+
+        galleryBtn = (Button) findViewById(R.id.galleryBtn);
+        galleryBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openGalleryScreen();
+            }
+        });
+
+        templatesBtn = (Button) findViewById(R.id.templatesBtn);
+        templatesBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openTemplateScreen();
+            }
+        });
+
+        importBtn = (Button) findViewById(R.id.importBtn);
+        importBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openImportScreen();
+            }
+        });
     }
     private final int CONFIG_REQUEST = 123;
 
     //Starts a new activity when user clicks
-    public void clickUnlock(View view)
-    {
-        Intent intent = new Intent(this,drawActivity.class);
-        startActivityForResult(intent, CONFIG_REQUEST);
-    }
-
-    public void clickUnlock(View view)
-    {
-        Intent intent = new Intent(this,galleryActivity.class);
-        startActivityForResult(intent, CONFIG_REQUEST);
-    }
-
-    public void clickUnlock(View view)
-    {
-        Intent intent = new Intent(this,TemplatesActivity.class);
-        startActivityForResult(intent, CONFIG_REQUEST);
-    }
-
-//    public void clickUnlock(View view)
+//    public void clickUnlock(View v)
 //    {
-//        Intent intent = new Intent(this,importActivity.class);
+//        Intent intent = new Intent(this,drawActivity.class);
 //        startActivityForResult(intent, CONFIG_REQUEST);
 //    }
+
+    public void openDrawScreen()
+     {
+        Intent intent = new Intent(this,  drawActivity.class);
+        startActivityForResult(intent, CONFIG_REQUEST);
+     }
+
+    public void openTemplateScreen()
+    {
+        Intent intent = new Intent(this,  TemplatesActivity.class);
+        startActivityForResult(intent, CONFIG_REQUEST);
+    }
+
+    public void openImportScreen()
+    {
+        Intent intent = new Intent(this,  importActivity.class);
+        startActivityForResult(intent, CONFIG_REQUEST);
+    }
+
+    public void openGalleryScreen()
+    {
+        Intent intent = new Intent(this,  galleryActivity.class);
+        startActivityForResult(intent, CONFIG_REQUEST);
+    }
 
 }
