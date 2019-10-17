644882178b9e489705f1917d99fefce1068a0a32
The-Great-Uniter <superdb117@gmail.com>
2019-10-17 14:02:24 -0400

    Worked on implimenting the recyclerview for choosing colors

--- app/src/main/java/com/example/colorfall/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/colorfall/RecyclerViewAdapter.java
@@ -1,24 +1,68 @@
 package com.example.colorfall;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerViewAdapter {
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
     private static final String TAG= "recyclerViewAdapter";
+    private ArrayList<Integer> ColorValues = new ArrayList<>();
+    private Context myContext;
 
-    public class viewHolder extends RecyclerView.ViewHolder {  //ViewHolder is a lib
+    //Default constructor
+    public RecyclerViewAdapter(Context Context, ArrayList<Integer> cVal) {
+        ColorValues = cVal;
+        myContext = Context;
+    }
 
-        ImageView image;
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_color,
+                parent, false);
+        RecyclerView.ViewHolder holder = new viewHolder(view); //Creates obj of viewHolder class
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called"); //Log for debugging
+
+        holder.colorValue.setColorFilter(ColorValues.get(position));
+
+        holder.parentLayout.setOnCLickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {}
+            //call brush setter here !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return ColorValues.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {  //.ViewHolder is a lib
+
+        //TextView colorName
+        ImageView colorValue;
         RelativeLayout parentLayout;
 
         public viewHolder(View itemView) {
             super(itemView);
-            image = itemView.findViewById(R.id.imageView);
-            parentLayout = itemView.findViewById(R.id.parent_layout)
+            colorValue = itemView.findViewById(R.id.imageView);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
         }
     }
 }
--- app/src/main/java/com/example/colorfall/drawActivity.java
+++ app/src/main/java/com/example/colorfall/drawActivity.java
@@ -1,29 +1,31 @@
 package com.example.colorfall;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-<<<<<<< HEAD
+
 import android.content.Context;
-=======
 import android.app.Activity;
->>>>>>> 53a76c63316daba8930f736d5a85d47b6ec0142d
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
 
 
 //this is the VIew in Model-View-Controller architecture
 public class drawActivity extends AppCompatActivity
 {
+    private ArrayList<Integer> colors = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-<<<<<<< HEAD
         //temp testing while save button DNE
         //drawView drawVie = new drawView(this);
         //drawVie.saveDrawing();
@@ -31,22 +33,37 @@ public class drawActivity extends AppCompatActivity
         //drawV.printSavedFiles();
         //above is testing while save btn DNE
 
-=======
         drawView drawView = new drawView(this);
         setContentView(R.layout.activity_draw);
->>>>>>> 53a76c63316daba8930f736d5a85d47b6ec0142d
+
+        colorData(); //Calls and creates color items for brush
     }
         //brush.setColor(Color.BLUE);
         //brush.setColor(Color.BLUE);
         //brush.setStyle(Paint.Style.STROKE);
         //brush.setStrokeJoin(Paint.Join.ROUND);
         //brush.setStrokeWidth(100f);
-    //Detect button press of color selector
-    public void colorSel(View view)
-    {
 
+    //Colors for brush
+    private void colorData()
+    {
+        colors.add(R.color.colorPrimary, 1);
+        colors.add(R.color.colorPrimary, 2);
+        //colors.add("Green");
+        //colors.add("Amber");
+       // colors.add("Deep Orange");
+
+        createRV();
     }
 
+    //Instantiates a recyclerview
+    private void createRV()
+    {
+        RecyclerView recyclerView = findViewById(R.id.cList);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, colors);
+        recyclerView.setAdapter(adapter); //Sets adapter to recyclerview
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 
     public void onClickSave (View view) {
         //drawView drawView = new drawView(this);
