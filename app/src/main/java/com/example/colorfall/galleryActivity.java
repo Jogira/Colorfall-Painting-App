3ba046eeac9e31aa01e28e072f1c48e952d03177
Jonathan <Zweitonegoismus@gmail.com>
2019-10-21 18:06:54 -0400

    Final Iteration One

--- app/src/main/java/com/example/colorfall/galleryActivity.java
+++ app/src/main/java/com/example/colorfall/galleryActivity.java
@@ -1,15 +1,14 @@
 package com.example.colorfall;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import android.widget.TextView;
 
 import com.mindorks.placeholderview.PlaceHolderView;
-
-//A work in progress for creating a gallery where we can view saved pictures.
-
+import android.content.Intent;
 
 
 
@@ -17,46 +16,48 @@ public class galleryActivity extends Activity {
     private PlaceHolderView mGalleryView;
     private String list;
 
-    private class galleryActivity1 extends Fragment {
+    //private class galleryActivity1 extends Fragment {
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
-
-    }
-};
 //
-    //
-    TextView textView = (TextView)findViewById(R.id.textView);
-    TextView m = (TextView) findViewById(R.id.textView);
-    TextView textView1 = (TextView)findViewById(R.id.textView);
-    TextView m1 = (TextView) findViewById(R.id.textView1);
-}
-            m.setBackgroundColor(Color.BLACK);
-                    m.setTextColor(Color.WHITE);
+        TextView textView = (TextView)findViewById(R.id.textView);
+        TextView m = (TextView) findViewById(R.id.textView);
+        TextView textView1 = (TextView)findViewById(R.id.textView);
+        TextView m1 = (TextView) findViewById(R.id.textView1);
+        m.setBackgroundColor(Color.BLACK);
+        m.setTextColor(Color.WHITE);
 
 
 
-                    drawActivity class1 = new drawActivity();
-                    String list1 = class1.getFiles();
+        drawActivity class1 = new drawActivity();
+        String list1 = class1.getFiles();
+
+
+//
+//            m.setText("Gallery");
+//
+        //mGalleryView = findViewById(R.id.galleryView);
+        //mGalleryView.addView(new GalleryItem(getResources().getDrawable(R.drawable.tempicon, null)));
+        Intent intent = getIntent();
+
+        list = intent.getStringExtra("filenames");
+        if(list!=null) {
+            m.setText("Gallery Contents:");
+            m1.setText(list);
 
 
 
-            m.setText("Gallery");
+        }
+    }
 
-                    mGalleryView = findViewById(R.id.galleryView);
-                    mGalleryView.addView(new GalleryItem(getResources().getDrawable(R.drawable.tempicon, null)));
-                    Intent intent = getIntent();
 
-                    list = intent.getStringExtra("filenames");
-                    if(list!=null) {
-                    m.setText("Gallery Contents:");
-                    m1.setText(list);
 
-                    }
-                    }
 
-                     }
+
+    // }
+
 }
