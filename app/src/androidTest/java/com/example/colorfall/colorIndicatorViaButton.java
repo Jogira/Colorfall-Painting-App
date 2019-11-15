cec421da2c73a595bd62c003ed3fba1608ad6820
The-Great-Uniter <superdb117@gmail.com>
2019-11-14 21:12:22 -0500

    Tried to impliment espresso test for choosing color

--- app/src/androidTest/java/com/example/colorfall/colorIndicatorViaButton.java
+++ app/src/androidTest/java/com/example/colorfall/colorIndicatorViaButton.java
@@ -1,12 +1,19 @@
 package com.example.colorfall;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import androidx.core.content.ContextCompat;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.filters.LargeTest;
+import androidx.test.internal.util.Checks;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
@@ -18,19 +25,24 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.InstrumentationRegistry.getTargetContext;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasBackground;
+import static androidx.test.espresso.matcher.ViewMatchers.hasTextColor;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class colorIndicatorViaButton {
+    int ourColor = 0x72f5f;
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
@@ -58,16 +70,20 @@ public class colorIndicatorViaButton {
                         isDisplayed()));
         appCompatImageButton.perform(click());
 
-        ViewInteraction imageButton = onView(
-                allOf(withId(R.id.current_Color),
-                        childAtPosition(
-                                allOf(withId(R.id.paint_colors2),
-                                        childAtPosition(
-                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                                5)),
-                                5),
-                        isDisplayed()));
-        imageButton.check(matches(isDisplayed()));
+//        ViewInteraction imageButton = onView(
+//                allOf(withId(R.id.current_Color),
+//                        childAtPosition(
+//                                allOf(withId(R.id.paint_colors2),
+//                                        childAtPosition(
+//                                                IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+//                                                5)),
+//                                5),
+//                        isDisplayed()));
+//        imageButton.check(matches(isDisplayed()));
+        //onView(withId(R.id.current_size)).check(matches(buttonShouldHaveBackgroundColor(ourColor)));
+        //onView(withId(R.id.current_size)).check(matches(buttonShouldHaveBackgroundColor(Color.RED)));
+
+        onView(withId(R.id.current_Color)).check(matches(hasBackground(ourColor)));
     }
 
     private static Matcher<View> childAtPosition(
